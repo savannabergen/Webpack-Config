5ebfe4f10a873f4b5f563a06013b71f3c2e56264
Savanna Bergen <savanna.bergen@gmail.com>
2024-06-27 09:08:32 -0500

    Configured SASS

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log((function(){return"I dont something"}))})();
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import generateJoke from './generateJoke'
+import './styles/main.scss'
+
 console.log(generateJoke())
